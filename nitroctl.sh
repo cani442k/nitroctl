47493bd26c702b62466d2a475249a6e8a9f69953
cani442k <cani442k@gmail.com>
2026-09-01 18:47:48 +0300

    Added GUI to launcher script

--- nitroctl.sh
+++ nitroctl.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 clear
-echo "Copyright (C) 2026  cani442k
+DBUS_REF=$(kdialog --progressbar "Copyright (C) 2026  cani442k
     This program comes with ABSOLUTELY NO WARRANTY.
     This is free software, and you are welcome to redistribute it
-    under certain conditions."
-sleep 2
-clear
+    under certain conditions." --title "nitroctl" 0 2>/dev/null) 
+qdbus $DBUS_REF showCancelButton false 2>/dev/null
 
-echo "Launching nitroctl..."
+sleep 3
+clear
+qdbus $DBUS_REF showCancelButton false 2>/dev/null
+qdbus $DBUS_REF setLabelText "Launching nitroctl..." 2>/dev/null
+
+sleep 1
+
+qdbus $DBUS_REF close 2>/dev/null
 if [ "$EUID" -ne 0 ]; then
-	echo "Root privilege required."
-	exec sudo "$0" "$@"
+	if [ -f "$HOME/.local/share/nitroctl/nitroctl.sh" ]; then
+		exec pkexec "$HOME/.local/share/nitroctl/nitroctl.sh" "$@"
+	else
+		exec pkexec "$0" "$@"
+	fi
 fi
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 cd "$SCRIPT_DIR/main" || exit 1
